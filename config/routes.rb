ef6e49327f99e793e09010cad3bd2fe9e6121c31
Forrest Reiland <you@example.com>
2020-10-23 16:33:45 -0700

    update devise.rb file so sign out works, test fail

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_scope :user do
-    get '/signout', to: 'devise/sessions#destroy', as: :signout
+  
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
   end
   root to: 'projects#index' 
   
