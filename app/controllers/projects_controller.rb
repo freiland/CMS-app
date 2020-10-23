e737432fa9eb7eccc5048fa82394623dce5e3339
Forrest Reiland <you@example.com>
2020-10-23 10:19:44 -0700

    add projects controller

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,53 @@
+class ProjectsController < ApplicationController
+
+
+  def index
+    @projects = Project.all
+    render :index
+  end
+
+  def new
+    @roject = Project.new
+    render :new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to projects_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+    render :edit
+  end
+
+  def show
+    @project = Project.find(params[:id])
+    render :show
+  end
+
+  def update
+    @project= Project.find(params[:id])
+    if @projec.update(projec_params)
+      redirect_to projects_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path
+  end
+
+  private
+    def project_params
+      params.require(:post).permit(:name, :author)
+    end
+
+  end
