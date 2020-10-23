007392976b6aca26695dcf895e11db5752fb4720
Forrest Reiland <you@example.com>
2020-10-23 10:12:19 -0700

    add comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,52 @@
+class CommentsController < ApplicationController
+
+
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = @posts.comments.new
+    render :new
+  end
+
+  def show
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    render :show
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@comment.post)
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to post_path(@comment.post)
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    render :edit
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+
+  private
+    def comment_params
+      params.require(:comment).permit(:author, :comment_body)
+    end
+end
