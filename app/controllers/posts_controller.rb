4a5f17fedb6b56eade2f40d56848c7fa8205f8a1
Forrest Reiland <you@example.com>
2020-10-23 16:03:55 -0700

    debud conditional logic to display admin information on projects#index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   before_action :only => [:new, :edit] do
     redirect_to new_user_session_path unless current_user && current_user.admin
+  end
+end
 
 
   def index
