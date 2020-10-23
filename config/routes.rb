89d2161c8701ee8d804b87c3ae680792c57d3673
Forrest Reiland <you@example.com>
2020-10-23 14:49:14 -0700

    add route to debug signout feature for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/signout', to: 'devise/sessions#destroy', as: :signout
+  end
   root to: 'projects#index' 
   
   resources :projects
