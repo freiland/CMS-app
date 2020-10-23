023fa6b83d0ef499c9aafaf2448edce3901b523b
Forrest Reiland <you@example.com>
2020-10-23 09:09:43 -0700

    add one to many relationship between posts and comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_155045) do
+ActiveRecord::Schema.define(version: 2020_10_23_160413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_10_23_155045) do
   create_table "comments", force: :cascade do |t|
     t.string "author"
     t.string "comment_body"
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
